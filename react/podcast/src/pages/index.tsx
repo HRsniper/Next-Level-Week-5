8be48fcd862becfd16dbf5c1637c9b246cb171f5
Hércules Ramlow (HRsniper) <63937242+HRsniper@users.noreply.github.com>
2021-04-21 15:19:32 -0400

    day 2 react

--- react/podcast/src/pages/index.tsx
+++ react/podcast/src/pages/index.tsx
@@ -1,11 +1,20 @@
-import { Header, Player } from "../components";
+import { GetStaticProps } from "next";
 
-function Home() {
-  return (
-    <main>
-      
-    </main>
-  );
+function Home({ episodes }) {
+  return <div>{JSON.stringify(episodes)}</div>;
 }
 
+const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  };
+};
+
 export default Home;
+export { getStaticProps };
